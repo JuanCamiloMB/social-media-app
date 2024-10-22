e99c1f1a36ddfc35f028d2652ac51b2451048b25
Juan Camilo Manjarrés Baena <63174756+JuanCamiloMB@users.noreply.github.com>
2024-10-22 11:10:10 -0500

    Update jest.config.ts

--- back/jest.config.ts
+++ back/jest.config.ts
@@ -2,7 +2,19 @@ import type { Config } from 'jest';
 
 const config: Config = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  roots: ['<rootDir>/tests'],  // Points to the tests directory
+  testRegex: '.*\\.test\\.ts$', // Regex to match test files
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',  // Use ts-jest to transform TypeScript files
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',  // Collect coverage from all JS and TS files
+  ],
+  coverageDirectory: './coverage',  // Coverage output directory
+  testEnvironment: 'node',  // Set environment to node for Express apps
+
 };
 
 export default config;
